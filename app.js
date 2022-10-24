d6dbad74bdee2c77d8574559ef354ea9e5a06616
guillaume <g.vautrin.it@gmail.fr>
2022-10-24 19:59:31 +0200

    Updated method to call for the server start

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ import { app, PORT } from './backend/server.js';
 
 
 // Lance l'écoute
-app.listen(PORT, () => console.log(`Server started (port ${PORT})`))
+app.listen(PORT, () => console.log(`Server started (port ${PORT})`));
