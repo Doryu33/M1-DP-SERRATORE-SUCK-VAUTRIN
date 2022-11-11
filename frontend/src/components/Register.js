6b60f898b984cbd16aae5d6bac49679415d0295d
suck5u <hugo.suck4@etu.univ-lorraine.fr>
2022-11-11 19:20:31 +0100

    coomit

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,9 +1,31 @@
-import React from 'react';
 import '../styles/register&login.css';
 import { Helmet } from "react-helmet";
 import Navigation from './Navigation';
+import React, { useState } from 'react';
+import network from '../configs/axiosParams';
 
 const Register = () => {
+
+    const [name, setName] = useState();
+    const [username, setUsername] = useState();
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+
+    const sendInfo = (e) => {
+        e.preventDefault();
+        console.log("senINfo");
+        const data = network.post(
+            "users/register",
+            {
+                username: username,
+                name: name,
+                email: email,
+                password: password,
+            }
+        );
+        console.log(data.id);
+    }
+
     return (
         <div>
             <Helmet>
@@ -11,54 +33,60 @@ const Register = () => {
             </Helmet>
             <Navigation />
             <form className="formRegister">
-                <div class="container">
-                    <h1 class="titleRegister">Créer un compte</h1>
+                <div className="container">
+                    <h1 className="titleRegister">Créer un compte</h1>
 
-                    <label for="name" className="labelInfo"><b>Prénom</b></label>
+                    <label htmlFor="name" className="labelInfo"><b>Prénom</b></label>
                     <input className="inputRegister"
                         type="name"
                         placeholder="Entrer votre prénom"
                         name="name"
                         id="name"
+                        onChange={(e) => setName(e.target.value)}
                         required
                     />
 
-                    <label for="username" className="labelInfo"><b>Pseudonyme</b></label>
+                    <label htmlFor="username" className="labelInfo"><b>Pseudonyme</b></label>
                     <input className="inputRegister"
                         type="text"
                         placeholder="Entrer votre pseudonyme"
                         name="username"
                         id="username"
+                        onChange={(e) => setUsername(e.target.value)}
                         required
                     />
 
-                    <label for="email" className="labelInfo"><b>Email</b></label>
+                    <label htmlFor="email" className="labelInfo"><b>Email</b></label>
                     <input className="inputRegister"
                         type="text"
                         placeholder="Entrer votre email"
                         name="email"
                         id="email"
+                        onChange={(e) => setEmail(e.target.value)}
                         required
                     />
 
-                    <label for="password" className="labelInfo"><b>Mot de passe</b></label>
+                    <label htmlFor="password" className="labelInfo"><b>Mot de passe</b></label>
                     <input className="inputRegister"
                         type="password"
                         placeholder="Entrer votre mot de passe"
                         name="password"
                         id="password"
+                        onChange={(e) => setPassword(e.target.value)}
                         required
                     />
 
-                    <button className="buttonRegister" type="submit">Terminer</button>
+                    <button className="buttonRegister" type="submit" onClick={(e) => sendInfo(e)}>Envoyer</button>
                 </div>
 
                 <div>
-                    <p class="pAlreadyAccount">Vous avez déjà un compte ? <a className="loginRedirection" href="http://localhost:3000/login">Connexion</a></p>
+                    <p className="pAlreadyAccount">Vous avez déjà un compte ? <a className="loginRedirection" href="http://localhost:3000/login">Connexion</a></p>
                 </div>
             </form>
         </div>
     );
 };
+
+
 
 export default Register;
