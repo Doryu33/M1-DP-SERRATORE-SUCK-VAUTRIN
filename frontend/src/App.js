8c25989f343ae75ca4bd9ac75644645085885a95
alex <alexandre.serratore@gmail.com>
2022-11-12 12:06:39 +0100

    Ajout début du calendar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,16 @@ import Register from './components/Register';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Login from './components/Login';
+import CustomCalendar from './components/CustomCalendar';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<CustomCalendar />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/calendar" element={<CustomCalendar/>} />
 
         {/*path="*" va vers l'accueil si l'url n'est pas reconnu*/}
         <Route path="*" element={<Login />} />
---
+++ frontend/src/components/CustomCalendar.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { useState } from 'react';
+import { Helmet } from "react-helmet";
+import Navigation from './Navigation';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import '../styles/customCalendar.css';
+
+const events = [
+    {
+        id: 1,
+        title: 'event 1',
+        start: '2022-11-12T20:00:00',
+        end: '2022-11-12T21:00:00',
+        backgroundColor: 'rgb(255,0,0)'
+    },
+    {
+        id: 2,
+        title: 'event 2',
+        start: '2022-11-20T20:00:00',
+        end: '2022-11-20T20:00:00',
+        backgroundColor: 'rgb(0,0,255)'
+    },
+];
+
+const CustomCalendar = () => {
+
+    const [date, setDate] = useState(new Date())
+    return (
+        <div>
+            <Helmet>
+                <title>Calendrier</title>
+            </Helmet>
+            <Navigation />
+            <div className="CustomCalendar" >
+                <FullCalendar
+                    plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+                    initialView="dayGridMonth"
+                    headerToolbar={{
+                        center: 'dayGridMonth,timeGridWeek,timeGridDay new',
+                    }}
+                    customButtons={{
+                        new: {
+                            text: 'new',
+                            click: () => console.log('new event'),
+                        },
+                    }}
+                    events={events}
+                    nowIndicator
+                    dateClick={(e) => console.log(e.dateStr)}
+                    eventClick={(e) => console.log(e.event.id)}
+                />
+            </div>
+            <div>
+                Selected date: {date.toDateString()}
+            </div>
+        </div>
+    );
+};
+
+export default CustomCalendar;
